e3e55963ae801c65fa1a004ce2ba473cfe2ca6f3
Björn Dahlgren <bjodah@gmail.com>
2018-08-02 16:50:27 +0200

    [WIP] Stub for migrating CI config

---
+++ .ci/run_ci.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+PKG_NAME=${1:-${CI_REPO##*/}}
+rm -r /usr/local/lib/python*/dist-packages/${PKG_NAME}*  # pip uninstall is useless
+set -e
+
+if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
+    eval export ${PKG_NAME^^}_RELEASE_VERSION=\$CI_BRANCH
+    echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
+fi
+
+for p in "${@:2}"
+do
+export CPATH=$p/include:$CPATH LIBRARY_PATH=$p/lib:$LIBRARY_PATH LD_LIBRARY_PATH=$p/lib:$LD_LIBRARY_PATH
+done
+
+git clean -xfd
+
+python3 setup.py sdist
+(cd dist/; python3 -m pip install $PKG_NAME-$(python3 ../setup.py --version).tar.gz)
+(cd /; python3 -m pytest --pyargs $PKG_NAME)
+(cd /; python3 -c "from pykinsol import get_include as gi; import os; assert 'kinsol_numpy.pxd' in os.listdir(gi())")
+
+CXX=clang++-6.0 CC=clang-6.0 CFLAGS='-fsanitize=address' python3 -m pip install --force-reinstall .
+
+
+PYTHONPATH=$(pwd) ./scripts/run_tests.sh
+(cd examples/; jupyter nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 *.ipynb)
+(cd examples/; ../scripts/render_index.sh *.html)
+
+
