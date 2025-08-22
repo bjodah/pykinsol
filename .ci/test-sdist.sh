60da7b3ef8fb6279a8b372d38e27c8e815269c64
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-08-23 00:34:56 +0200

    tweak env vars

--- .ci/test-sdist.sh
+++ .ci/test-sdist.sh
@@ -2,13 +2,15 @@
 set -euxo pipefail
 source $(compgen -G /opt-3/cpython-v3.*-apt-deb/bin/activate)
 git clean -xfd
-python3 setup.py sdist
-cd ./dist/
 SUNDBASE=$(compgen -G /opt-3/sundials-6.*-release)
-env \
+( export \
     CFLAGS="-isystem $SUNDBASE/include -isystem /usr/include/suitesparse" \
     LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib" \
-    CC=gcc CXX=g++ pip install ${CI_REPO_NAME}-*.tar.gz
+    CC=gcc CXX=g++ \
+    ; python3 setup.py sdist \
+    ; cd ./dist/ \
+    ; pip install ${CI_REPO_NAME}-*.tar.gz )
+
 python3 -m pytest --pyargs pykinsol
 cd -
 env \
