8b5afe3078329510ce124a6850a4adbb4b1dd6a7
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-08-23 00:28:05 +0200

    CI tweaks

---
+++ .ci/test-sdist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euxo pipefail
+source $(compgen -G /opt-3/cpython-v3.*-apt-deb/bin/activate)
+git clean -xfd
+python3 setup.py sdist
+cd ./dist/
+SUNDBASE=$(compgen -G /opt-3/sundials-6.*-release)
+env \
+    CFLAGS="-isystem $SUNDBASE/include -isystem /usr/include/suitesparse" \
+    LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib" \
+    CC=gcc CXX=g++ pip install ${CI_REPO_NAME}-*.tar.gz
+python3 -m pytest --pyargs pykinsol
+cd -
+env \
+    CFLAGS="-isystem $SUNDBASE/include -isystem /usr/include/suitesparse" \
+    LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib" \
+    CC=gcc CXX=g++ python3 setup.py build_ext -i
+
+pip install pytest-flakes pytest-cov matplotlib sphinx numpydoc sphinx-rtd-theme
+./scripts/run_tests.sh --cov ${CI_REPO_NAME} --cov-report html
+./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
+./scripts/generate_docs.sh
